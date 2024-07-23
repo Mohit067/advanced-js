401008ec6f6e8b6a911a6f21387fb54f8b8f6016
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-07-23 06:01:55 +0000

    methods

---
+++ ObjectMethod/ObjectvariableAccess.js
@@ -0,0 +1,13 @@
+let product = {
+    name : "i phone 14 pro max",
+    company : "Apple",
+    price : 124000,
+    color : "red"
+}
+console.log(Object.keys(product)); // get all key present in object 
+
+console.log(Object.values(product)); // get all value present in object
+
+console.log(Object.entries(product)); // get all key value pair
+
+console.log(Object.entries(product).length); // get length of key value pair

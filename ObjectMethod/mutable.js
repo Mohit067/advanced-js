958c0756f6ed9ac929511cc627cffc7d887a928e
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-07-23 06:14:30 +0000

    mutability

---
+++ ObjectMethod/mutable.js
@@ -0,0 +1,10 @@
+let product = {
+    name : "i phone 14 pro max",
+    company : "Apple",
+    price : 124000,
+    color : "red"
+}
+product.name = "i phone 14";
+console.log(product.name); // here name is change so we can say that (let) is mutable
+
+// basically mutable  means we can change value of variable after declaration, and modify and delete variable

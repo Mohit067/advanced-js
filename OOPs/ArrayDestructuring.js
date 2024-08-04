9177f3d3bddfb5663b0a1193dc54e8483d7e7b9f
mohitsahu60067 <mohitsahu60067@gmail.com>
2024-08-04 08:34:34 +0000

    Arrat Destructure added

---
+++ OOPs/ArrayDestructuring.js
@@ -0,0 +1,16 @@
+const arr = [100 , 99 , 98 , 97 , 96];
+const [math , sccienc , english , hindi , sanskrit] = arr;
+console.log(math);
+console.log(sccienc);
+// console.log(english);
+// console.log(hindi);
+// console.log(sanskrit);
+
+
+const  arr1 = [10 , function () {}];
+const [value , setValue] = arr1;
+console.log(value);
+console.log(setValue);
+
+const arr2 = [1,2,3, ...arr, arr1];
+console.log(arr2);
